193fbe2ff3f07e27af49976b8b11e12995f62e7a
Anders Eriksson <36226327+blixten85@users.noreply.github.com>
2026-08-21 19:01:53 +0200

    Point app shell at unified stylesheet

--- app/public/components/app-shell.js
+++ app/public/components/app-shell.js
@@ -1,6 +1,6 @@
 import { installSendJobsController } from "./send-jobs.js";
 
-const stylesheet = "/ui-refresh.css";
+const stylesheet = "/app-shell.css";
 if (!document.querySelector(`link[href="${stylesheet}"]`)) {
   const link = document.createElement("link");
   link.rel = "stylesheet";
